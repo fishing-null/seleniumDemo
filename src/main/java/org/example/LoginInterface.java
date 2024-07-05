c3513f07c9c6eb1f899694a7d8e4d9d5d07154c5
MatouSakura <1134979484@qq.com>
2024-07-05 11:16:37 +0800

    模拟用户登录类

--- src/main/java/org/example/LoginInterface.java
+++ src/main/java/org/example/LoginInterface.java
@@ -1,5 +1,6 @@
 package org.example;
 
 public interface LoginInterface {
-    //所有角色的登录通过这个接口拓展
+    //所有角色的登录通过这个接口拓展 通过输入账号和密码登录
+    void login(String account, String password) throws InterruptedException;
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,8 +5,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
     public static void main(String[] args) {
-        WebDriver webDriver = new ChromeDriver();
-
 
     }
 }
